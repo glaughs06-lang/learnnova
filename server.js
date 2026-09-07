3e4a6ca8a5096a1588a286402828f9b977925500
glaughs06-lang <glaughs06@gmail.com>
2026-09-07 17:44:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
+app.use(express.static(path.join(__dirname)));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
